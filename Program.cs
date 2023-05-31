72381abe67f9518fa8729457269569e158f4d57b
Štěpán Velínský <stteranv@gmail.com>
2023-06-01 01:25:03 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, spočítám ti průměr známek. Kolik známek zadáš ? ");
+            int pocet = int.Parse(Console.ReadLine());
+            int[] cisla = new int[pocet];
+            for (int i = 0; i < pocet; i++)
+            {
+                Console.Write("Zadejte {0}. číslo: ", i + 1);
+                cisla[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Průměr tvých známek je: {0}", cisla.Average());
+            Console.ReadKey();
+        }
+    }
+}
